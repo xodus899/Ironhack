3e7755243e6bc477fc37f5b219605054a1397c2c
Christopher Justice <justcd@mail.broward.edu>
2016-08-16 14:57:41 -0400

    creating an array for cities

--- week1/carsounds1.rb
+++ week1/carsounds1.rb
@@ -2,16 +2,44 @@ class Car
 	def initialize(name,sound)
 		@name = name
 		@sound= sound
-	end
+
+		@city_array = []
+		end
+
 	def car_sounds
 		puts "#{@name} has taken off #{@sound}"
-
 	end
+
+	def city_push(city_name)
+		@city_array.push(city_name)
+	end
+
+	def cities_traveled
+		@city_array
+	end
+
+
+		
+
 end
 
 ferrari = Car.new("Ferrari","vrooooooooom , SCREEEECCCCH!!")
-pontiac = Car.new("Pontiac","Baaaa Ba ba ba baaaaaaa!!")
-
 ferrari.car_sounds
+
 puts "" + "and car 2 is not far behind!!"
+
+pontiac = Car.new("Pontiac","Baaaa Ba ba ba baaaaaaa!!")
 pontiac.car_sounds
+puts ""
+
+pontiac.city_push("Miami")
+pontiac.city_push("Bozeman")
+ferrari.city_push("Raleigh")
+ferrari.city_push("Hollywood")
+
+
+puts "These cars have made it to:"
+puts "Pontiac"
+p pontiac.cities_traveled
+puts "Ferrari"
+p ferrari.cities_traveled
