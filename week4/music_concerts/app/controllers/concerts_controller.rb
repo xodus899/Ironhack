9cef072e29103f73bb701ac77cc2742710363476
Christopher Justice <justcd@mail.broward.edu>
2016-09-12 14:56:50 -0400

    add detail page for concerts

--- week4/music_concerts/app/controllers/concerts_controller.rb
+++ week4/music_concerts/app/controllers/concerts_controller.rb
@@ -2,8 +2,37 @@ class ConcertsController < ApplicationController
 
 	def index 
 		@concert = Concert.all
-		
-		render :index
 
+		render :index
 	end 
+
+	def new
+		@create_concert = Concert.new
+
+		
+	end
+
+	def create
+
+		@created_concert = Concert.new(
+			name:        params[:concert][:name],
+			artist:      params[:concert][:artist],
+			venue: 		 params[:concert][:venue],
+			city:  		 params[:concert][:city],
+			date:  		 params[:concert][:date],
+			price:  	 params[:concert][:price],
+			description: params[:concert][:description] )
+
+
+
+		@created_concert.save
+		redirect_to("/concerts")
+
+	end
+
+	def show
+		@one_concert = Concert.find(params[:id])
+
+		render :show
+	end
 end
