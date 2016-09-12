ff8c666631679a045c86eebac5e9affe1054e28b
Christopher Justice <justcd@mail.broward.edu>
2016-09-12 11:32:55 -0400

    created new project with seeds/module/controller

---
+++ week4/music_concerts/db/migrate/20160912145654_create_concerts.rb
@@ -0,0 +1,15 @@
+class CreateConcerts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :concerts do |t|
+      t.string :name
+      t.string :artist
+      t.string :venue
+      t.string :city
+      t.datetime :date
+      t.integer :price
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
---
+++ week4/music_concerts/db/seeds.rb
@@ -0,0 +1,4 @@
+concert1 = concert.create!(name: " Iron Hack ", artist: " Nizar ", venue: " Bb&T Center  ", city: " Sunrise,Fl ", date: Date.today + 1.week, price: 40, description: " Snow to go on account dead men tell no tales ")
+concert2 = concert.create!(name: " Magic hat ", artist: " Dancing Daniel ", venue: " American Air Lines Arena ", city: " Miami, Fl ", date: Date.today, price: 45, description: "A comely wench doing my nut in sweet fanny adams jammy git crumpets flog a dead horse" )
+concert3 = concert.create!(name: " Mystic Dolls", artist: " Jenny Sparkles" , venue: " Dalian Arena ", city: "  Dalian ", date: Date.today , price: 20 , description: " Your bones don't break, mine do " )
+concert4 = concert.create!(name: " Diamond Boom ", artist: " Jewel Theif ", venue: " Staples Center ", city: " Los Angeles ", date: Date.today + 2.week , price: 10,  description: " Pastry gingerbread carrot cake muffin ")
